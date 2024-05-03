c0fc3d81fb86b789caf021f57a3c8e1ad2a1c19c
J Cole Morrison <me@jcolemorrison.com>
2024-05-02 20:59:59 -0700

    k8s and apps setup

--- applications/variables.tf
+++ applications/variables.tf
@@ -1,14 +1,14 @@
 variable "project_name" {
   type        = string
   description = "The name of the project.  Used for naming resources."
-  default     = "hashistack-k8s"
+  default     = "hashistack-apps"
 }
 
 variable "aws_default_tags" {
   type        = map(string)
   description = "Default tags added to all AWS resources."
   default = {
-    Project = "hashistack-k8s"
+    Project = "hashistack-apps"
   }
 }
 
@@ -21,15 +21,4 @@ variable "aws_default_region" {
 variable "eks_cluster_name" {
   type        = string
   description = "The name of the EKS cluster."
-}
-
-variable "aws_lb_controller_version" {
-  type        = string
-  description = "The version of the AWS Load Balancer Controller."
-  default     = "1.7.2"
-}
-
-variable "aws_lb_controller_role_arn" {
-  type        = string
-  description = "The ARN of the IAM role for the AWS Load Balancer Controller."
 }
---
+++ kubernetes/providers.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.47.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.29.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_default_region
+  default_tags {
+    tags = var.aws_default_tags
+  }
+}
+
+# Alternatively you could pass these values via workspaces, variables, TFE provider, or TF State
+data "aws_eks_cluster" "main" {
+  name = var.eks_cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.us_west_2.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.us_west_2.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.us_west_2.token
+  }
+}
