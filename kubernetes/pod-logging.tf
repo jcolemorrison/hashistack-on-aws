14a4e00e8c902dc7cdd55e9c7c71ae7bf97e891f
J Cole Morrison <me@jcolemorrison.com>
2024-05-04 13:28:41 -0700

    add back fluent bit

--- kubernetes/pod-logging.tf
+++ kubernetes/pod-logging.tf
@@ -14,40 +14,35 @@ resource "kubernetes_namespace" "aws_for_fluent_bit" {
 #   }
 # }
 
-# resource "helm_release" "aws_for_fluent_bit" {
-#   name       = "aws-for-fluent-bit"
-#   chart      = "aws-for-fluent-bit"
-#   version    = "0.1.32"
-#   repository = "https://aws.github.io/eks-charts"
-#   namespace  = kubernetes_namespace.aws_for_fluent_bit.metadata[0].name
+resource "helm_release" "aws_for_fluent_bit" {
+  name       = "aws-for-fluent-bit"
+  chart      = "aws-for-fluent-bit"
+  version    = "0.1.32"
+  repository = "https://aws.github.io/eks-charts"
+  namespace  = kubernetes_namespace.aws_for_fluent_bit.metadata[0].name
 
-#   set {
-#     name  = "cloudwatch.region"
-#     value = var.aws_default_region
-#   }
+  set {
+    name  = "cloudwatch.region"
+    value = var.aws_default_region
+  }
 
-#   set {
-#     name  = "cloudwatch.log_group_name"
-#     value = var.pod_cloudwatch_log_group_name
-#   }
+  set {
+    name  = "cloudwatch.logGroupName"
+    value = var.pod_cloudwatch_log_group_name
+  }
 
-#   set {
-#     name  = "cloudwatch.log_stream_prefix"
-#     value = "aws-for-fluent-bit"
-#   }
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
 
-#   set {
-#     name  = "serviceAccount.create"
-#     value = "true"
-#   }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-for-fluent-bit"
+  }
 
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "aws-for-fluent-bit"
-#   }
-
-#   set {
-#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-#     value = var.pod_cloudwatch_logging_arn
-#   }
-# }
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = var.pod_cloudwatch_logging_arn
+  }
+}
