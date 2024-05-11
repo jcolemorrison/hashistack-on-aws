4d1f66b575ba15bbff14808144f92edb7e05b8ce
J Cole Morrison <me@jcolemorrison.com>
2024-05-11 14:52:06 -0700

    remove region arg

--- infrastructure/hcp-consul.tf
+++ infrastructure/hcp-consul.tf
@@ -1,7 +1,7 @@
 resource "hcp_consul_cluster" "main" {
   cluster_id         = var.project_name
   hvn_id             = hcp_hvn.main.hvn_id
-  region             = var.hcp_consul_region
+  # region             = var.hcp_consul_region
   min_consul_version = var.hcp_consul_version
   tier               = var.hcp_consul_tier
   public_endpoint    = var.hcp_consul_public_endpoint
