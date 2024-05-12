ce46c5a0531e8610887b0cb99cecd7df1e454a4b
J Cole Morrison <me@jcolemorrison.com>
2024-05-12 12:42:07 -0700

    allow 443 from hvn

--- infrastructure/hcp-consul.tf
+++ infrastructure/hcp-consul.tf
@@ -27,6 +27,16 @@ resource "aws_security_group_rule" "hcp_consul_udp" {
   description       = "Allow Consul UDP traffic from HCP HVN"
 }
 
+resource "aws_security_group_rule" "hcp_consul_tcp_443" {
+  security_group_id = aws_eks_cluster.cluster.vpc_config.0.cluster_security_group_id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = [hcp_hvn.main.cidr_block]
+  description       = "Allow Consul TCP traffic from HCP HVN"
+}
+
 # Security Group Rules - Egress
 
 resource "aws_security_group_rule" "hcp_consul_tcp_egress_8300" {
