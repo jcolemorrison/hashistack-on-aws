b128688131c7f762d780b9b0e8d98005e99ab824
J Cole Morrison <me@jcolemorrison.com>
2024-05-11 19:57:10 -0700

    use consul cluster id

--- packages/outputs.tf
+++ packages/outputs.tf
@@ -1,10 +1,10 @@
 data "hcp_consul_agent_helm_config" "main" {
-  cluster_id          = var.eks_cluster_name
+  cluster_id          = var.hcp_consul_cluster_id
   kubernetes_endpoint = var.eks_cluster_api_endpoint
 }
 
 data "hcp_consul_agent_kubernetes_secret" "main" {
-  cluster_id = var.eks_cluster_name
+  cluster_id = var.hcp_consul_cluster_id
 }
 
 output "hcp_consul_agent_helm_config" {
