1232b579180c3f346480df6575bb4ba48dc2a616
J Cole Morrison <me@jcolemorrison.com>
2024-05-12 23:08:54 -0700

    add ssl redirect

--- services/user-interface.tf
+++ services/user-interface.tf
@@ -16,6 +16,7 @@ resource "kubernetes_manifest" "ingress_ui" {
         "alb.ingress.kubernetes.io/target-type" = "ip"
         "alb.ingress.kubernetes.io/subnets" = join(",", local.public_subnet_ids)
         "alb.ingress.kubernetes.io/listen-ports" = "[{\"HTTP\": 80}, {\"HTTPS\":443}]"
+        "alb.ingress.kubernetes.io/ssl-redirect" = "443"
       }
     }
     spec = {
