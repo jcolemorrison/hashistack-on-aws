5215d486b79f1b6f11233bcf2539812a760d3964
J Cole Morrison <me@jcolemorrison.com>
2024-05-12 22:27:08 -0700

    only use annotations

--- dns/user-interface.tf
+++ dns/user-interface.tf
@@ -5,12 +5,12 @@ resource "kubernetes_annotations" "ui_alb_ssl" {
     name = "ui"
     namespace = "ui"
   }
-  # These annotations will be applied to the Deployment resource itself
+
   annotations = {
     "alb.ingress.kubernetes.io/certificate-arn" = aws_acm_certificate.ui_subdomain.arn
   }
   # These annotations will be applied to the Pods created by the Deployment
-  template_annotations = {
-    "alb.ingress.kubernetes.io/certificate-arn" = aws_acm_certificate.ui_subdomain.arn
-  }
+  # template_annotations = {
+  #   "alb.ingress.kubernetes.io/certificate-arn" = aws_acm_certificate.ui_subdomain.arn
+  # }
 }
