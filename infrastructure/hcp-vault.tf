ce459a038f14ebdc5d08024f748e2189cc03be34
J Cole Morrison <me@jcolemorrison.com>
2024-05-17 21:01:35 -0700

    add sg for injector

--- infrastructure/hcp-vault.tf
+++ infrastructure/hcp-vault.tf
@@ -5,6 +5,16 @@ resource "hcp_vault_cluster" "main" {
   public_endpoint = var.hcp_vault_public_endpoint
 }
 
+resource "aws_security_group_rule" "hcp_vault_tcp_8080" {
+  security_group_id = aws_eks_cluster.cluster.vpc_config.0.cluster_security_group_id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 8080
+  to_port           = 8080
+  self              = true
+  description       = "Allow Vault Injector TCP traffic from HCP HVN to EKS"
+}
+
 resource "aws_security_group_rule" "hcp_vault_tcp_egress_8200" {
   security_group_id = aws_eks_cluster.cluster.vpc_config.0.cluster_security_group_id
   type              = "egress"
