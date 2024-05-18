8dc75e3b6defe29dec26f9673d17ef10f1acb798
J Cole Morrison <me@jcolemorrison.com>
2024-05-18 16:20:07 -0700

    vault service account and auth

--- packages/hcp-vault.tf
+++ packages/hcp-vault.tf
@@ -23,3 +23,41 @@ resource "helm_release" "vault" {
 
   depends_on = [aws_eks_addon.vpc_cni]
 }
+
+# Service account for pods to authenticate with Vault
+data "kubernetes_service_account" "vault" {
+  metadata {
+    name      = "vault"
+    namespace = kubernetes_namespace.vault.metadata.0.name
+  }
+
+  depends_on = [helm_release.vault]
+}
+
+resource "kubernetes_secret" "vault_sa_token" {
+  metadata {
+    name      = "vault"
+    namespace = helm_release.vault.namespace
+    annotations = {
+      "kubernetes.io/service-account.name"      = data.kubernetes_service_account.vault.metadata.0.name
+      "kubernetes.io/service-account.namespace" = data.kubernetes_service_account.vault.metadata.0.namespace
+    }
+  }
+
+  type = "kubernetes.io/service-account-token"
+
+  depends_on = [helm_release.vault]
+}
+
+# Enable Kubernetes auth method
+resource "vault_auth_backend" "kubernetes" {
+  type = "kubernetes"
+}
+
+resource "vault_kubernetes_auth_backend_config" "kubernetes" {
+  backend                = vault_auth_backend.kubernetes.path
+  kubernetes_host        = var.kubernetes_endpoint
+  kubernetes_ca_cert     = kubernetes_secret.vault_sa_token.data["ca.crt"]
+  token_reviewer_jwt     = var.kubernetes_token
+  disable_iss_validation = "true"
+}
