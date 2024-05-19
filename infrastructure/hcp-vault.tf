120b91a35335d1829cd6088f1d9bf22209745f01
J Cole Morrison <me@jcolemorrison.com>
2024-05-19 13:04:52 -0700

    remove output for refresh

--- infrastructure/hcp-vault.tf
+++ infrastructure/hcp-vault.tf
@@ -5,6 +5,8 @@ resource "hcp_vault_cluster" "main" {
   public_endpoint = var.hcp_vault_public_endpoint
 }
 
+# May need to be refreshed every 6 hours due to known issue:
+# https://registry.terraform.io/providers/hashicorp/hcp/latest/docs/resources/vault_cluster_admin_token
 # resource "hcp_vault_cluster_admin_token" "bootstrap" {
 #   cluster_id = hcp_vault_cluster.main.cluster_id
 # }
