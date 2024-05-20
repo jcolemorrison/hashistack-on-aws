8bf560c3de715f5b4f606aba2459fa0b01856c15
J Cole Morrison <me@jcolemorrison.com>
2024-05-20 00:54:32 -0700

    correct stack check

--- services/checks.tf
+++ services/checks.tf
@@ -3,7 +3,7 @@ check "ui_load_balancer_active" {
     tags = {
       "elbv2.k8s.aws/cluster"    = local.eks_cluster_name
       "ingress.k8s.aws/resource" = "LoadBalancer"
-      "ingress.k8s.aws/stack"    = "ui/ui"
+      "ingress.k8s.aws/stack"    = "default/ui"
     }
   }
   assert {
