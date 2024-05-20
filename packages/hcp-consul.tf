5a3d3fda8931686dca12fb1e6c14902c262473de
J Cole Morrison <me@jcolemorrison.com>
2024-05-19 22:27:09 -0700

    add proxy defaults

--- packages/hcp-consul.tf
+++ packages/hcp-consul.tf
@@ -119,3 +119,23 @@ resource "consul_config_entry" "products" {
     Protocol    = "http"
   })
 }
+
+resource "consul_config_entry" "proxy_defaults" {
+  kind      = "proxy-defaults"
+  name      = "global"
+  partition = "default"
+
+  config_json = jsonencode({
+    AccessLogs = {
+      Enabled = true
+    }
+    Expose = {}
+    MeshGateway = {
+      Mode = "local"
+    }
+    TransparentProxy = {}
+    Config = {
+      Protocol = "http"
+    }
+  })
+}
