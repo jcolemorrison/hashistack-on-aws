5406335b58e700bd6e9dbf752a31e4870996c54d
J Cole Morrison <me@jcolemorrison.com>
2024-05-19 17:43:29 -0700

    correct upstream dns lookup

--- services/user-interface.tf
+++ services/user-interface.tf
@@ -135,7 +135,7 @@ resource "kubernetes_manifest" "deployment_ui" {
                 },
                 {
                   name  = "UPSTREAM_URIS"
-                  value = "http://products.service.products.dc1.consul"
+                  value = "http://products.service.products.hashistack.consul"
                 }
                 # {
                 #   name  = "MESSAGE"
