2e80d1029af9c078c057a0012a2b4a8d957de03a
J Cole Morrison <me@jcolemorrison.com>
2024-05-27 23:37:25 -0700

    self on boundary security group

--- infrastructure/hcp-boundary.tf
+++ infrastructure/hcp-boundary.tf
@@ -108,6 +108,15 @@ resource "aws_security_group_rule" "allow_9202_worker" {
   security_group_id = aws_security_group.boundary_worker.id
 }
 
+resource "aws_security_group_rule" "allow_9202_self_worker" {
+  type              = "ingress"
+  from_port         = 9202
+  to_port           = 9202
+  protocol          = "tcp"
+  self              = true
+  security_group_id = aws_security_group.boundary_worker.id
+}
+
 resource "aws_security_group_rule" "allow_22_debug" {
   type              = "ingress"
   from_port         = 22
