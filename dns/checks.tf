9a057e816770ccc4d5689b9b5ebfa3b55f6ba885
J Cole Morrison <me@jcolemorrison.com>
2024-10-06 16:25:07 -0700

    manual remove of dns resources

--- dns/checks.tf
+++ dns/checks.tf
@@ -1,10 +1,10 @@
-check "ui_service_live" {
-  data "http" "ui" {
-    url = "https://${var.public_subdomain_name}"
-  }
+# check "ui_service_live" {
+#   data "http" "ui" {
+#     url = "https://${var.public_subdomain_name}"
+#   }
 
-  assert {
-    condition = data.http.ui.status_code == 200
-    error_message = "UI service should return a 200 code"
-  }
-}
+#   assert {
+#     condition = data.http.ui.status_code == 200
+#     error_message = "UI service should return a 200 code"
+#   }
+# }
