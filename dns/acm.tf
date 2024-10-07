c459a089b771782206dac0fa108b7d9b0dab82b7
J Cole Morrison <me@jcolemorrison.com>
2024-10-07 14:57:48 -0700

    add back dns functions

--- dns/acm.tf
+++ dns/acm.tf
@@ -1,32 +1,32 @@
-# resource "aws_acm_certificate" "ui_subdomain" {
-#   domain_name               = var.public_subdomain_name
-#   validation_method         = "DNS"
-#   subject_alternative_names = [var.public_subdomain_name]
+resource "aws_acm_certificate" "ui_subdomain" {
+  domain_name               = var.public_subdomain_name
+  validation_method         = "DNS"
+  subject_alternative_names = [var.public_subdomain_name]
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_acm_certificate_validation" "ui_subdomain" {
-#   certificate_arn         = aws_acm_certificate.ui_subdomain.arn
-#   validation_record_fqdns = [for record in aws_route53_record.ui_subdomain_validation : record.fqdn]
-# }
+resource "aws_acm_certificate_validation" "ui_subdomain" {
+  certificate_arn         = aws_acm_certificate.ui_subdomain.arn
+  validation_record_fqdns = [for record in aws_route53_record.ui_subdomain_validation : record.fqdn]
+}
 
-# # Validation for subdomain certificate
-# resource "aws_route53_record" "ui_subdomain_validation" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.ui_subdomain.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+# Validation for subdomain certificate
+resource "aws_route53_record" "ui_subdomain_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.ui_subdomain.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = data.aws_route53_zone.public_domain_name.zone_id
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.public_domain_name.zone_id
+}
--- dns/route53.tf
+++ dns/route53.tf
@@ -3,22 +3,22 @@ data "aws_route53_zone" "public_domain_name" {
   name = var.public_domain_name
 }
 
-# data "aws_lb" "ui" {
-#   tags = {
-#     "elbv2.k8s.aws/cluster"    = local.eks_cluster_name
-#     "ingress.k8s.aws/resource" = "LoadBalancer"
-#     "ingress.k8s.aws/stack"    = local.ui_stack_name
-#   }
-# }
+data "aws_lb" "ui" {
+  tags = {
+    "elbv2.k8s.aws/cluster"    = local.eks_cluster_name
+    "ingress.k8s.aws/resource" = "LoadBalancer"
+    "ingress.k8s.aws/stack"    = local.ui_stack_name
+  }
+}
 
-# output "ui_lb_dns_name" {
-#   value = data.aws_lb.ui.dns_name
-# }
+output "ui_lb_dns_name" {
+  value = data.aws_lb.ui.dns_name
+}
 
-# resource "aws_route53_record" "ui" {
-#   zone_id = data.aws_route53_zone.public_domain_name.zone_id
-#   name    = var.public_subdomain_name
-#   type    = "CNAME"
-#   ttl     = 60
-#   records = [data.aws_lb.ui.dns_name]
-# }
+resource "aws_route53_record" "ui" {
+  zone_id = data.aws_route53_zone.public_domain_name.zone_id
+  name    = var.public_subdomain_name
+  type    = "CNAME"
+  ttl     = 60
+  records = [data.aws_lb.ui.dns_name]
+}
