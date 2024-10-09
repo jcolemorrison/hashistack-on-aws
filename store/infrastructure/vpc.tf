cc76587f8ae5ef813a18e46bcc33ba6b60559301
J Cole Morrison <me@jcolemorrison.com>
2024-10-08 23:42:45 -0700

    correct path to vpc module

--- store/infrastructure/vpc.tf
+++ store/infrastructure/vpc.tf
@@ -1,6 +1,6 @@
 # VPC
 module "vpc" {
-  source     = "../modules/vpc"
+  source     = "../../modules/vpc"
   cidr_block = local.global_vpc_cidr_blocks["store_us_east_1"]
   name       = var.project_name
   transit_gateway_id = local.transit_gateway_id
