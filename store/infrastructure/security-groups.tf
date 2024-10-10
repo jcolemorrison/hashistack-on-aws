cbee5ad1080eec069456435140e039fda1cc90a6
J Cole Morrison <me@jcolemorrison.com>
2024-10-10 01:40:40 -0700

    modify eks security group rules

--- store/infrastructure/security-groups.tf
+++ store/infrastructure/security-groups.tf
@@ -9,5 +9,15 @@ resource "aws_security_group_rule" "eks_remote_access_ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = [local.global_vpc_cidr_blocks["store_us_east_1"]]
+  # cidr_blocks       = [local.global_vpc_cidr_blocks["store_us_east_1"]]
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "eks_remote_access_ssh_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_remote_access.id
 }
