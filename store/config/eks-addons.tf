b2060b46098c00acaad134a37b0af6b5f0ad471c
J Cole Morrison <me@jcolemorrison.com>
2024-10-10 03:32:54 -0700

    add aws eks blueprint addons

--- store/config/eks-addons.tf
+++ store/config/eks-addons.tf
@@ -12,3 +12,23 @@ module "aws_loadbalancer_controller" {
   eks_cluster_oidc_provider_arn = local.eks_cluster_oidc_provider_arn
   eks_cluster_oidc_provider_url = local.eks_cluster_oidc_provider_url
 }
+
+module "eks_blueprints_addons" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "1.16.2"
+
+  cluster_name      = data.aws_eks_cluster.main.id
+  cluster_endpoint  = data.aws_eks_cluster.main.endpoint
+  cluster_version   = data.aws_eks_cluster.main.version
+  oidc_provider_arn = local.eks_cluster_oidc_provider_arn
+
+  enable_metrics_server = true
+  metrics_server = {
+    wait = true
+  }
+
+  enable_cluster_autoscaler = true
+  cluster_autoscaler = {
+    wait = true
+  }
+}
