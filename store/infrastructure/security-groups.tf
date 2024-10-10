820919ea02759aa9494546e2ec1ad59bf1c4a2ef
J Cole Morrison <me@jcolemorrison.com>
2024-10-10 02:32:04 -0700

    note about eks remote access updates

--- store/infrastructure/security-groups.tf
+++ store/infrastructure/security-groups.tf
@@ -9,8 +9,12 @@ resource "aws_security_group_rule" "eks_remote_access_ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
+
+  # Note: in latest updates to EKS, the only thing that matters is the security group
+  # being attached to whatever needs to access the EKS nodes.  SSH access into the nodes
+  # is on an auto generated security group that points to this one as a source.  Therefore
+  # the CIDR block isn't needed.
   cidr_blocks       = [local.global_vpc_cidr_blocks["store_us_east_1"]]
-  # cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "eks_remote_access_ssh_egress" {
