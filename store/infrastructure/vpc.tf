d106aba7d645ff5767be106cdbb012b1efac6ad4
J Cole Morrison <me@jcolemorrison.com>
2024-10-10 03:59:31 -0700

    add hvn to transit gateway route tables

--- store/infrastructure/vpc.tf
+++ store/infrastructure/vpc.tf
@@ -4,5 +4,8 @@ module "vpc" {
   cidr_block             = local.global_vpc_cidr_blocks["store_us_east_1"]
   name                   = var.project_name
   transit_gateway_id     = local.transit_gateway_id
-  accessible_cidr_blocks = [for cidr in values(local.global_vpc_cidr_blocks) : cidr if cidr != local.global_vpc_cidr_blocks["store_us_east_1"]]
+    accessible_cidr_blocks = concat(
+    [for cidr in values(local.global_vpc_cidr_blocks) : cidr if cidr != local.global_vpc_cidr_blocks["store_us_east_1"]],
+    [local.hcp_hvn_cidr_block]
+  )
 }
