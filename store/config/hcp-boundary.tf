3161e75d49c91112d155626cdfa6c716780f6034
J Cole Morrison <me@jcolemorrison.com>
2024-10-10 00:49:57 -0700

    remove boundary workers

--- store/config/hcp-boundary.tf
+++ store/config/hcp-boundary.tf
@@ -1,14 +1,14 @@
-module "boundary_worker" {
-  source                          = "../../modules/config/boundary/worker"
-  aws_region                      = var.aws_default_region
-  boundary_worker_ec2_kepair_name = var.hcp_boundary_ec2_key_pair_name
-  vpc_public_subnet_ids           = local.store_vpc_public_subnet_ids
-  vpc_id                          = local.store_vpc_id
-  boundary_address                = local.hcp_boundary_address
-  project_name                    = var.project_name
-  boundary_worker_count           = 2
-  allow_debug_ssh                 = true
-}
+# module "boundary_worker" {
+#   source                          = "../../modules/config/boundary/worker"
+#   aws_region                      = var.aws_default_region
+#   boundary_worker_ec2_kepair_name = var.hcp_boundary_ec2_key_pair_name
+#   vpc_public_subnet_ids           = local.store_vpc_public_subnet_ids
+#   vpc_id                          = local.store_vpc_id
+#   boundary_address                = local.hcp_boundary_address
+#   project_name                    = var.project_name
+#   boundary_worker_count           = 2
+#   allow_debug_ssh                 = true
+# }
 
 # module "boundary_eks_node_targets" {
 #   source                                = "../../modules/config/boundary/eks-node-targets"
