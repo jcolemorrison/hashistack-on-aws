66a8550bb43c5feedb645b9de2024c1e94b8ce06
J Cole Morrison <me@jcolemorrison.com>
2024-10-10 14:53:30 -0700

    add back route53 acm for correct subdomain

--- store/config/outputs.tf
+++ store/config/outputs.tf
@@ -1,4 +1,4 @@
-# output "subdomain_certificate_arn" {
-#   value       = module.service_subdomain_certificate.acm_certificate_arn
-#   description = "The ARN of the ACM certificate for the subdomain"
-# }
+output "subdomain_certificate_arn" {
+  value       = module.service_subdomain_certificate.acm_certificate_arn
+  description = "The ARN of the ACM certificate for the subdomain"
+}
