8653f69f29738411180928b83fe19df4036fb038
J Cole Morrison <me@jcolemorrison.com>
2024-10-10 01:19:29 -0700

    correct remote state values

--- store/config/locals.tf
+++ store/config/locals.tf
@@ -1,8 +1,13 @@
 locals {
+  # outputs from store workspaces
   eks_cluster_name                   = try(data.terraform_remote_state.store_infrastructure.outputs.eks_cluster_name, var.eks_cluster_name)
   eks_node_group_name                = try(data.terraform_remote_state.store_infrastructure.outputs.eks_node_group_name, var.eks_node_group_name)
   store_vpc_id                       = try(data.terraform_remote_state.store_infrastructure.outputs.vpc_id, var.store_vpc_id)
   store_vpc_public_subnet_ids        = try(data.terraform_remote_state.store_infrastructure.outputs.vpc_public_subnet_ids, var.store_vpc_public_subnet_ids)
+  hcp_boundary_access_key_id        = try(data.terraform_remote_state.store_infrastructure.outputs.hcp_boundary_access_key_id, var.hcp_boundary_access_key_id)
+  hcp_boundary_secret_access_key    = try(data.terraform_remote_state.store_infrastructure.outputs.hcp_boundary_secret_access_key, var.hcp_boundary_secret_access_key)
+
+  # outputs from global workspaces
   hcp_hvn_id                         = try(data.terraform_remote_state.infrastructure.outputs.hcp_hvn_id, var.hcp_hvn_id)
   hcp_consul_public_endpoint         = try(data.terraform_remote_state.infrastructure.outputs.hcp_consul_public_endpoint, var.hcp_consul_public_endpoint)
   hcp_consul_cluster_id              = try(data.terraform_remote_state.infrastructure.outputs.hcp_consul_cluster_id, var.hcp_consul_cluster_id)
@@ -15,6 +20,4 @@ locals {
   hcp_boundary_login_name            = try(data.terraform_remote_state.infrastructure.outputs.hcp_boundary_login_name, var.hcp_boundary_login_name)
   hcp_boundary_login_pwd             = try(data.terraform_remote_state.infrastructure.outputs.hcp_boundary_login_pwd, var.hcp_boundary_login_pwd)
   hcp_boundary_hashistack_project_id = try(data.terraform_remote_state.config.outputs.hcp_boundary_hashistack_project_id, var.hcp_boundary_hashistack_project_id)
-  hcp_boundary_access_key_id        = try(data.terraform_remote_state.infrastructure.outputs.hcp_boundary_access_key_id, var.hcp_boundary_access_key_id)
-  hcp_boundary_secret_access_key    = try(data.terraform_remote_state.infrastructure.outputs.hcp_boundary_secret_access_key, var.hcp_boundary_secret_access_key)
 }
