31a0e82fde80b5a996526a4b49746cbe29c15afa
J Cole Morrison <me@jcolemorrison.com>
2024-10-09 18:21:09 -0700

    use correct locals for module

--- store/config/hcp-boundary.tf
+++ store/config/hcp-boundary.tf
@@ -1,9 +1,9 @@
 module "boundary_worker" {
   source                          = "../../modules/config/boundary/worker"
   aws_region                      = var.aws_default_region
-  vpc_public_subnet_ids           = var.store_vpc_public_subnet_ids
   boundary_worker_ec2_kepair_name = var.hcp_boundary_ec2_key_pair_private_key
-  vpc_id                          = var.store_vpc_id
+  vpc_public_subnet_ids           = local.store_vpc_public_subnet_ids
+  vpc_id                          = local.store_vpc_id
   boundary_address                = local.hcp_boundary_address
   project_name                    = var.project_name
   boundary_worker_count           = 2
