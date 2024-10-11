6a738ea39253adfb3a66cd14d7d27f27af2806c0
J Cole Morrison <me@jcolemorrison.com>
2024-10-11 06:16:18 -0700

    add social config for eks

--- social/config/eks-addons.tf
+++ social/config/eks-addons.tf
@@ -7,12 +7,13 @@ module "pod_logging" {
 }
 
 # TBD: error with http hop limit
-# module "aws_loadbalancer_controller" {
-#   source                        = "../../modules/config/eks-pod-lb"
-#   eks_cluster_name              = local.eks_cluster_name
-#   eks_cluster_oidc_provider_arn = local.eks_cluster_oidc_provider_arn
-#   eks_cluster_oidc_provider_url = local.eks_cluster_oidc_provider_url
-# }
+module "aws_loadbalancer_controller" {
+  source                        = "../../modules/config/eks-pod-lb"
+  eks_cluster_name              = local.eks_cluster_name
+  eks_cluster_oidc_provider_arn = local.eks_cluster_oidc_provider_arn
+  eks_cluster_oidc_provider_url = local.eks_cluster_oidc_provider_url
+  vpc_id                        = local.vpc_id
+}
 
 module "eks_blueprints_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
--- social/config/hcp-consul.tf
+++ social/config/hcp-consul.tf
@@ -3,4 +3,5 @@ module "hcp_consul_eks" {
   eks_cluster_api_endpoint   = local.eks_cluster_api_endpoint
   hcp_consul_cluster_id      = local.hcp_consul_cluster_id
   hcp_consul_bootstrap_token = local.hcp_consul_bootstrap_token
+  project_name               = var.project_name
 }
