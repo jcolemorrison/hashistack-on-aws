4c5b2a1fd852253165b54c5bb45a86a7ae8b195e
J Cole Morrison <me@jcolemorrison.com>
2024-10-11 03:44:40 -0700

    add consul uri for game service

--- game/services/outputs.tf
+++ game/services/outputs.tf
@@ -1,7 +1,12 @@
-output "public_service_lb_tags" {
-  value = {
-    "elbv2.k8s.aws/cluster"    = local.eks_cluster_name
-    "ingress.k8s.aws/resource" = "LoadBalancer"
-    "ingress.k8s.aws/stack"    = module.game.service_lb_stack_name
-  }
+# output "public_service_lb_tags" {
+#   value = {
+#     "elbv2.k8s.aws/cluster"    = local.eks_cluster_name
+#     "ingress.k8s.aws/resource" = "LoadBalancer"
+#     "ingress.k8s.aws/stack"    = module.game.service_lb_stack_name
+#   }
+# }
+
+output "game_service_uri" {
+  value = module.game.service_uri
+  description = "internal consul uri for the game service"
 }
