8a89890fb4250aab9ecc5d354ac527cd28e4b2e7
J Cole Morrison <me@jcolemorrison.com>
2024-10-11 04:18:11 -0700

    reset consul in the store

--- store/config/hcp-consul.tf
+++ store/config/hcp-consul.tf
@@ -1,6 +1,6 @@
-module "hcp_consul_eks" {
-  source = "../../modules/config/consul/eks"
-  eks_cluster_api_endpoint = local.eks_cluster_api_endpoint
-  hcp_consul_cluster_id = local.hcp_consul_cluster_id
-  hcp_consul_bootstrap_token = local.hcp_consul_bootstrap_token
-}
+# module "hcp_consul_eks" {
+#   source = "../../modules/config/consul/eks"
+#   eks_cluster_api_endpoint = local.eks_cluster_api_endpoint
+#   hcp_consul_cluster_id = local.hcp_consul_cluster_id
+#   hcp_consul_bootstrap_token = local.hcp_consul_bootstrap_token
+# }
--- store/services/main.tf
+++ store/services/main.tf
@@ -1,14 +1,14 @@
-module "products" {
-  source                  = "../../modules/services/eks/internal"
-  service_name            = "products"
-  service_vault_auth_path = local.vault_kubernets_auth_path
-}
+# module "products" {
+#   source                  = "../../modules/services/eks/internal"
+#   service_name            = "products"
+#   service_vault_auth_path = local.vault_kubernets_auth_path
+# }
 
-module "store" {
-  source                  = "../../modules/services/eks/public"
-  service_name            = "store"
-  service_vault_auth_path = local.vault_kubernets_auth_path
-  public_subnet_ids       = local.store_vpc_public_subnet_ids
-  acm_certificate_arn     = local.subdomain_certificate_arn
-  upstream_uris           = [module.products.service_uri, "http://game.virtual.consul"]
-}
+# module "store" {
+#   source                  = "../../modules/services/eks/public"
+#   service_name            = "store"
+#   service_vault_auth_path = local.vault_kubernets_auth_path
+#   public_subnet_ids       = local.store_vpc_public_subnet_ids
+#   acm_certificate_arn     = local.subdomain_certificate_arn
+#   upstream_uris           = [module.products.service_uri, "http://game.virtual.consul"]
+# }
