146bbbafa0b37348318bc7ccdf4a6f71bad00eb0
J Cole Morrison <me@jcolemorrison.com>
2024-10-11 05:05:21 -0700

    increase desired node size

--- game/infrastructure/runtime.tf
+++ game/infrastructure/runtime.tf
@@ -5,4 +5,5 @@ module "eks_cluster" {
   private_subnet_ids                  = module.vpc.private_subnet_ids
   eks_remote_access_ec2_kepair_name   = var.ec2_kepair_name
   eks_remote_access_security_group_id = aws_security_group.eks_remote_access.id
+  node_group_desired_size             = 3
 }
