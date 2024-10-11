c408cb83d5f38e106b8aff83907ee131bfbcb20b
J Cole Morrison <me@jcolemorrison.com>
2024-10-10 18:32:15 -0700

    actually add the acm cert local

--- store/services/locals.tf
+++ store/services/locals.tf
@@ -3,4 +3,5 @@ locals {
   eks_cluster_name            = try(data.terraform_remote_state.store_infrastructure.outputs.eks_cluster_name, var.eks_cluster_name)
   vault_kubernets_auth_path   = try(data.terraform_remote_state.store_config.outputs.vault_kubernets_auth_path, var.vault_kubernets_auth_path)
   store_vpc_public_subnet_ids = try(data.terraform_remote_state.store_infrastructure.outputs.vpc_public_subnet_ids, var.store_vpc_public_subnet_ids)
+  subdomain_certificate_arn   = try(data.terraform_remote_state.store_config.outputs.subdomain_certificate_arn, var.subdomain_certificate_arn)
 }
