8edd5eabe7dc002a1d6dcbe214702fb7653b7a6c
J Cole Morrison <me@jcolemorrison.com>
2024-10-11 05:46:21 -0700

    add back k8s auth path

--- game/config/outputs.tf
+++ game/config/outputs.tf
@@ -3,7 +3,7 @@ output "subdomain_certificate_arn" {
   description = "The ARN of the ACM certificate for the subdomain"
 }
 
-# output "vault_kubernets_auth_path" {
-#   value       = module.hcp_vault_eks.vault_kubernetes_auth_path
-#   description = "Path to the Kubernetes auth method in Vault on EKS"
-# }
+output "vault_kubernets_auth_path" {
+  value       = module.hcp_vault_eks.vault_kubernetes_auth_path
+  description = "Path to the Kubernetes auth method in Vault on EKS"
+}
