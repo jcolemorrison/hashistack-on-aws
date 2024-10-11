4a8e92ca6ed18a4d24ba44a5c75885ba46dbc0d2
J Cole Morrison <me@jcolemorrison.com>
2024-10-11 02:20:14 -0700

    reset game dns

--- game/config/route53.tf
+++ game/config/route53.tf
@@ -1,6 +1,6 @@
-module "service_subdomain_certificate" {
-  source = "../../modules/config/route53/acm-subdomain"
+# module "service_subdomain_certificate" {
+#   source = "../../modules/config/route53/acm-subdomain"
 
-  public_subdomain_name = local.subdomain_name
-  subdomain_zone_id     = local.subdomain_zone_id
-}
+#   public_subdomain_name = local.subdomain_name
+#   subdomain_zone_id     = local.subdomain_zone_id
+# }
