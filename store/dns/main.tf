b41831ee19ef4ac2a07ad6596fabc2a6b9a7277f
J Cole Morrison <me@jcolemorrison.com>
2024-10-10 19:27:39 -0700

    use local

--- store/dns/main.tf
+++ store/dns/main.tf
@@ -1,6 +1,6 @@
 # Main Route53 hosted zone - requires that you've set this up through UI, CLI, or separate TF project
 data "aws_route53_zone" "public_subdomain_name" {
-  name = var.subdomain_name
+  name = local.subdomain_name
 }
 
 data "aws_lb" "lb" {
@@ -9,7 +9,7 @@ data "aws_lb" "lb" {
 
 resource "aws_route53_record" "public_domain" {
   zone_id = data.aws_route53_zone.public_subdomain_name.zone_id
-  name    = var.subdomain_name
+  name    = local.subdomain_name
   type    = "CNAME"
   ttl     = 60
   records = [data.aws_lb.lb.dns_name]
