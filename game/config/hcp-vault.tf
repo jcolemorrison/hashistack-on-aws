3ae95210ff45b3bc42c4973384e0eec44cc84dcd
J Cole Morrison <me@jcolemorrison.com>
2024-10-11 05:24:22 -0700

    add back eks addons to game cluster

--- game/config/hcp-vault.tf
+++ game/config/hcp-vault.tf
@@ -1,6 +1,6 @@
-module "hcp_vault_eks" {
-  source                   = "../../modules/config/vault/eks"
-  project_name             = var.project_name
-  eks_cluster_api_endpoint = local.eks_cluster_api_endpoint
-  vault_private_endpoint   = local.hcp_vault_private_endpoint
-}
+# module "hcp_vault_eks" {
+#   source                   = "../../modules/config/vault/eks"
+#   project_name             = var.project_name
+#   eks_cluster_api_endpoint = local.eks_cluster_api_endpoint
+#   vault_private_endpoint   = local.hcp_vault_private_endpoint
+# }
