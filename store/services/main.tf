986022f937a6bc132e4c484fb3e7a4ab87d73c1a
J Cole Morrison <me@jcolemorrison.com>
2024-10-11 23:06:06 -0700

    add customers service

--- store/services/main.tf
+++ store/services/main.tf
@@ -4,11 +4,17 @@ module "products" {
   service_vault_auth_path = local.vault_kubernets_auth_path
 }
 
+module "customers" {
+  source                  = "../../modules/services/eks/internal"
+  service_name            = "customers"
+  service_vault_auth_path = local.vault_kubernets_auth_path
+}
+
 module "store" {
   source                  = "../../modules/services/eks/public"
   service_name            = "store"
   service_vault_auth_path = local.vault_kubernets_auth_path
   public_subnet_ids       = local.store_vpc_public_subnet_ids
   acm_certificate_arn     = local.subdomain_certificate_arn
-  upstream_uris           = [module.products.service_uri, "http://comments.virtual.consul"]
+  upstream_uris           = [module.products.service_uri, module.customers.service_uri, "http://comments.virtual.consul"]
 }
