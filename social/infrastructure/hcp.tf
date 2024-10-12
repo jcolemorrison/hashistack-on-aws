3dc2aa7db12778479b7102e3dc3ce7ff0901687a
J Cole Morrison <me@jcolemorrison.com>
2024-10-11 23:28:13 -0700

    remove duplicate sg rules

--- social/infrastructure/hcp.tf
+++ social/infrastructure/hcp.tf
@@ -34,27 +34,7 @@ resource "aws_security_group_rule" "hcp_consul_udp" {
   description       = "Allow Consul UDP traffic from HCP HVN"
 }
 
-resource "aws_security_group_rule" "allow_ingress_store_us_east_1_cidr" {
-  security_group_id = module.eks_cluster.eks_cluster_security_group_id
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 0
-  to_port           = 65535
-  cidr_blocks       = [local.global_vpc_cidr_blocks["store_us_east_1"]]
-  description       = "Allow traffic from the store VPC"
-}
-
 ## Security Group Rules - Egress
-
-resource "aws_security_group_rule" "allow_egress_store_us_east_1_cidr" {
-  security_group_id = module.eks_cluster.eks_cluster_security_group_id
-  type              = "egress"
-  protocol          = "tcp"
-  from_port         = 0
-  to_port           = 65535
-  cidr_blocks       = [local.global_vpc_cidr_blocks["store_us_east_1"]]
-  description       = "Allow traffic from the store VPC"
-}
 
 resource "aws_security_group_rule" "hcp_consul_tcp_egress_8300" {
   security_group_id = module.eks_cluster.eks_cluster_security_group_id
