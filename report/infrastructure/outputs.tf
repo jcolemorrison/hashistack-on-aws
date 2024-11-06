ee10d05e2e5d9144bc30d7783d54ea5e5f0cd33a
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-11-06 11:10:02 -0500

    Add report infrastructure with Nomad cluster

---
+++ report/infrastructure/outputs.tf
@@ -0,0 +1,34 @@
+output "vpc_id" {
+  value       = module.vpc.id
+  description = "ID of the VPC"
+}
+
+output "vpc_public_subnet_ids" {
+  value       = module.vpc.public_subnet_ids
+  description = "List of public subnet IDs"
+}
+
+output "vpc_private_subnet_ids" {
+  value       = module.vpc.private_subnet_ids
+  description = "List of private subnet IDs"
+}
+
+output "hcp_boundary_access_key_id" {
+  value = aws_iam_access_key.boundary.id
+}
+
+output "hcp_boundary_secret_access_key" {
+  value     = aws_iam_access_key.boundary.secret
+  sensitive = true
+}
+
+output "nomad_endpoint" {
+  value       = module.nomad_server.nomad_endpoint
+  description = "Nomad server endpoint"
+}
+
+output "nomad_token" {
+  value       = module.nomad_server.nomad_management_token
+  description = "Nomad bootstrap token"
+  sensitive   = true
+}
---
+++ report/infrastructure/runtime.tf
@@ -0,0 +1,22 @@
+# Nomad servers
+module "nomad_server" {
+  source                               = "../../modules/infrastructure/nomad/server"
+  project_name                         = var.project_name
+  vpc_id                               = module.vpc.id
+  private_subnets                      = module.vpc.private_subnet_ids
+  public_subnets                       = module.vpc.public_subnet_ids
+  default_aws_region                   = var.aws_default_region
+  nomad_remote_access_ec2_keypair_name = var.ec2_keypair_name
+}
+
+module "nomad_node_pool" {
+  source                               = "../../modules/infrastructure/nomad/node-pool"
+  project_name                         = var.project_name
+  name                                 = "standard"
+  vpc_id                               = module.vpc.id
+  private_subnets                      = module.vpc.private_subnet_ids
+  default_aws_region                   = var.aws_default_region
+  nomad_remote_access_ec2_keypair_name = var.ec2_keypair_name
+  security_group_ids                   = [module.nomad_server.nomad_security_group_id, aws_security_group.nomad_remote_access.id]
+  node_pool_desired_size               = 3
+}
