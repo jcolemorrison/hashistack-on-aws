a10ac02aac2894e39e5c63a97d346d66b80c7e0e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-11-22 11:16:57 -0500

    Output Boundary bucket access keys

--- modules/config/boundary/session-recording/outputs.tf
+++ modules/config/boundary/session-recording/outputs.tf
@@ -1,3 +1,12 @@
 output "boundary_session_recording_policy_arn" {
   value = aws_iam_policy.boundary.arn
 }
+
+output "boundary_bucket_access_key_id" {
+  value = aws_iam_access_key.boundary.id
+}
+
+output "boundary_bucket_secret_access_key" {
+  value     = aws_iam_access_key.boundary.secret
+  sensitive = true
+}
