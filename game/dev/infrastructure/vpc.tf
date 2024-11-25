eeb89374e5a7ded109c8b2e443406bc4761b95a9
J Cole Morrison <me@jcolemorrison.com>
2024-11-24 16:08:42 -0800

    use existing hvn route

--- game/dev/infrastructure/vpc.tf
+++ game/dev/infrastructure/vpc.tf
@@ -1,11 +1,11 @@
 # VPC
 module "vpc" {
   source                 = "../../modules/vpc"
-  cidr_block             = local.global_vpc_cidr_blocks["game_us_east_1_dev"]
+  cidr_block             = local.global_vpc_cidr_blocks["game_us_west_2"]
   name                   = "dev-${var.project_name}"
   transit_gateway_id     = local.transit_gateway_id
   accessible_cidr_blocks = concat(
-    [for cidr in values(local.global_vpc_cidr_blocks) : cidr if cidr != local.global_vpc_cidr_blocks["game_us_east_1_dev"]],
+    [for cidr in values(local.global_vpc_cidr_blocks) : cidr if cidr != local.global_vpc_cidr_blocks["game_us_west_2"]],
     [local.hcp_hvn_cidr_block]
   )
 }
