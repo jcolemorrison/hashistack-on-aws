dc74644754d9d91d36bdd94fb7b7b971b64e7ff4
J Cole Morrison <me@jcolemorrison.com>
2024-11-25 09:43:18 -0800

    cv check bug

--- game/dns/checks.tf
+++ game/dns/checks.tf
@@ -4,7 +4,7 @@ check "check_game_dns" {
   }
 
   assert {
-    condition = data.http.game_lb_dns.status_code == 200
+    condition = data.http.game_lb_dns.status_code == 404
     error_message = format("Game DNS should return a 200 status code, instead of `%s`",
       data.http.game_lb_dns.status_code
     )
